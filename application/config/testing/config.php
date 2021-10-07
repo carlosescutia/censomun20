1bde04ee7d87a61d81fcc9a90f186929d0924dca
Carlos Escutia <carlosescutia@gmail.com>
2021-10-07 12:22:44 -0500

    Se modifica base_url de testing a tstcensomun20.guanajuato.gob.mx.

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pubcensomun20.guanajuato.gob.mx/';
+$config['base_url'] = 'http://tstcensomun20.guanajuato.gob.mx/';
 
 /*
 |--------------------------------------------------------------------------
