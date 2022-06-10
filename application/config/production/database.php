477db410e9b1e905c2d66457e706381fee5f44ea
Carlos Escutia <carlosescutia@gmail.com>
2022-06-10 12:43:53 -0500

    Se modifica configuración de base de datos de producción.

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'postgres',
 	'username' => 'censomun20',
 	'password' => 'censomun20',
 	'database' => 'censomun20',
